38641eb09bc639dc6f953e84213520d4197f693f
VictorSdePasta <victor.delima@sptech.school>
2026-02-23 12:24:09 -0300

    inicio backend cadastro conectado com o banco

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -2,27 +2,37 @@ const pt1 = document.getElementById('divParte1')
 const pt2 = document.getElementById('divParte2')
 const cnpjsCadastrados = []
 
-let inpRazao = document.getElementById('inptRazaoSocial').value
-let inpCnpj = document.getElementById('inptCNPJ').value
-let inpCep = document.getElementById('inptCEP').value
-let inpEndereco = document.getElementById('inptEndereco').value
-let inpNumero = document.getElementById('inptNumero').value
-let inpComplemento = document.getElementById('inptComplemento').value
+let inpRazao = document.getElementById('inptRazaoSocial')
+let inpCnpj = document.getElementById('inptCNPJ')
+let inpCep = document.getElementById('inptCEP')
+let inpEndereco = document.getElementById('inptEndereco')
+let inpNumero = document.getElementById('inptNumero')
+let inpComplemento = document.getElementById('inptComplemento')
 
 function proximo() {
-    inpRazao = document.getElementById('inptRazaoSocial').value
-    inpCnpj = document.getElementById('inptCNPJ').value
-    inpCep = document.getElementById('inptCEP').value
-    inpEndereco = document.getElementById('inptEndereco').value
-    inpNumero = document.getElementById('inptNumero').value
-    inpComplemento = document.getElementById('inptComplemento').value
+    inpRazao = document.getElementById('inptRazaoSocial')
+    inpCnpj = document.getElementById('inptCNPJ')
+    inpCep = document.getElementById('inptCEP')
+    inpEndereco = document.getElementById('inptEndereco')
+    inpNumero = document.getElementById('inptNumero')
+    inpComplemento = document.getElementById('inptComplemento')
 
-    if (inpRazao.trim() != "" && inpCnpj.trim().length == 14 && inpCep.trim().length == 8 && inpEndereco.trim() != "" && inpNumero.trim() != "") {
-        pt1.style.display = 'none'
-        pt2.style.display = 'flex'
-        return
+    let cnpj = inpCnpj.value.replaceAll('.','')
+    cnpj = cnpj.replaceAll('/','')
+    cnpj = cnpj.replaceAll('-','')
+
+    let cep = inpCep.value.replaceAll('-','')
+
+    if (inpRazao.value.trim() != "" && cnpj.trim().length == 14 && cep.trim().length == 8 && inpEndereco.value.trim() != "" && inpNumero.value.trim() != "") {
+        if (!buscarPorCnpj(inpCnpj.value)) {
+            pt1.style.display = 'none'
+            pt2.style.display = 'flex'
+            return
+        } else {
+            'cnpj já cadastrado'
+            return
+        }
     }
-
     erro()
 }
 
@@ -31,20 +41,21 @@ function voltar() {
     pt1.style.display = 'flex'
 }
 
-let inpNome = document.getElementById('inptNome').value
-let inpEmail = document.getElementById('inptEmail').value
-let inpCelular = document.getElementById('inptCelular').value
-let inpSenha = document.getElementById('inptSenha').value
-let inpConfirma = document.getElementById('inptConfirmaSenha').value
+let inpNome = document.getElementById('inptNome')
+let inpEmail = document.getElementById('inptEmail')
+let inpCelular = document.getElementById('inptCelular')
+let inpSenha = document.getElementById('inptSenha')
+let inpConfirma = document.getElementById('inptConfirmaSenha')
 
 function cadastrar() {
-    inpNome = document.getElementById('inptNome').value
-    inpEmail = document.getElementById('inptEmail').value
-    inpCelular = document.getElementById('inptCelular').value
-    inpSenha = document.getElementById('inptSenha').value
-    inpConfirma = document.getElementById('inptConfirmaSenha').value
+    inpNome = document.getElementById('inptNome')
+    inpEmail = document.getElementById('inptEmail')
+    inpCelular = document.getElementById('inptCelular')
+    inpSenha = document.getElementById('inptSenha')
+    inpConfirma = document.getElementById('inptConfirmaSenha')
 
-    if (inpNome != "" && inpEmail != "" && inpCelular != "" && inpSenha != "" && inpSenha == inpConfirma) {
+    if (inpNome.value != "" && inpEmail.value != "" && inpCelular.value != "" && inpSenha.value != "" && inpSenha.value == inpConfirma.value) {
+
 
         setTimeout(() => {
             window.location = "login.html";
@@ -53,4 +64,82 @@ function cadastrar() {
     }
 
     erro()
+}
+
+function erro() {
+    document.getElementById('divFundoErro').style.display = 'flex'
+
+    if (inpRazao.value == '') {
+        document.getElementById('spnRazao').style.color = 'red'
+    } else {
+        document.getElementById('spnRazao').style.color = 'white'
+    }
+    if (inpCnpj.value == '') {
+        document.getElementById('spnCnpj').style.color = 'red'
+    } else {
+        document.getElementById('spnCnpj').style.color = 'white'
+    }
+    if (inpCep.value == '') {
+        document.getElementById('spnCep').style.color = 'red'
+    } else {
+        document.getElementById('spnCep').style.color = 'white'
+    }
+    if (inpEndereco.value == '') {
+        document.getElementById('spnEndereco').style.color = 'red'
+    } else {
+        document.getElementById('spnEndereco').style.color = 'white'
+    }
+    if (inpNumero.value == '') {
+        document.getElementById('spnNumero').style.color = 'red'
+    } else {
+        document.getElementById('spnNumero').style.color = 'white'
+    }
+    if (inpNome.value == '') {
+        document.getElementById('spnNome').style.color = 'red'
+    } else {
+        document.getElementById('spnNome').style.color = 'white'
+    }
+    if (inpEmail.value == '') {
+        document.getElementById('spnEmail').style.color = 'red'
+    } else {
+        document.getElementById('spnEmail').style.color = 'white'
+    }
+    if (inpCelular.value == '') {
+        document.getElementById('spnCelular').style.color = 'red'
+    } else {
+        document.getElementById('spnCelular').style.color = 'white'
+    }
+    if (inpSenha.value == '') {
+        document.getElementById('spnSenha').style.color = 'red'
+    } else {
+        document.getElementById('spnSenha').style.color = 'white'
+    }
+    if (inpConfirma.value == '') {
+        document.getElementById('spnConfirma').style.color = 'red'
+    } else {
+        document.getElementById('spnConfirma').style.color = 'white'
+    }
+
+    setTimeout(() => {
+        document.getElementById('divFundoErro').style.display = 'none'
+    }, "2000");
+    return
+}
+
+function buscarPorCnpj(cnpj) {
+    fetch("/empresas/buscar", {
+        method: "GET",
+    })
+        .then(function (resposta) {
+            resposta.json().then((resposta) => {
+                if (resposta.length > 0) {
+                    return false
+                } else {
+                    return true
+                }
+            });
+        })
+        .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+        });
 }
