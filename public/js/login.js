a243cbd4ad108180a80dbdc846186a8927ab52b7
VictorSdePasta <victor.delima@sptech.school>
2026-03-05 22:05:11 -0300

    código para apresentação sem conexão com banco de dados

--- public/js/login.js
+++ public/js/login.js
@@ -2,26 +2,46 @@ async function login() {
     let email = document.getElementById('inptEmail').value
     let senha = document.getElementById('inptSenha').value
 
-    const resposta = await fetch('/usuarios/autenticar', {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            emailServer: email,
-            senhaServer: senha
-        })
-    })
+    // const resposta = await fetch('/usuarios/autenticar', {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //         emailServer: email,
+    //         senhaServer: senha
+    //     })
+    // })
 
-    if (resposta.ok) {
-        let resp = resposta.json()
-        sessionStorage.EMAIL_USUARIO = resp.email;
-        sessionStorage.NOME_USUARIO = resp.nome;
-        sessionStorage.ID_USUARIO = resp.id;
+    // if (resposta.ok) {
+    //     let resp = resposta.json()
+    //     sessionStorage.EMAIL_USUARIO = resp.email;
+    //     sessionStorage.NOME_USUARIO = resp.nome;
+    //     sessionStorage.ID_USUARIO = resp.id;
 
-        setTimeout(() => {
-            window.location = "./dashboard/dashboard.html";
-        }, "2000");
+    //     setTimeout(() => {
+    //         window.location = "./dashboard/dashboard.html";
+    //     }, "2000");
+    //     return
+    // }
+    
+    if (email.trim() != "" && email != null && senha.trim() != "" && senha != null) {
+        erro("3000", "Login realizado com sucesso")
+
+        // setTimeout(() => {
+        //     window.location = "./dashboard/dashboard.html";
+        // }, "2000");
         return
     }
 
-    console.log('erro interno')
+    erro("3000", "Usuário e/ou Senha incorretos")
+}
+
+
+function erro(tempo, texto) {
+    document.getElementById('divFundoErro').style.display = 'flex'
+
+setTimeout(() => {
+        document.getElementById('spnErro').innerHTML = texto
+        document.getElementById('divFundoErro').style.display = 'none'
+    }, tempo);
+    return
 }
