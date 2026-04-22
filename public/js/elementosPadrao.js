27f51ac4f575bac972cfe79435ecdceda0e191cf
Caiqe <cttcaiquegomes@gmail.com>
2026-04-22 17:18:58 -0300

    Feat: iniciando padronização

--- public/js/elementosPadrao.js
+++ public/js/elementosPadrao.js
@@ -4,3 +4,14 @@ const header = document.getElementById("header");
 toggle.addEventListener("change", function () {
     header.classList.toggle("ativo");
 });
+
+const pegarNomeUsuario = () =>{
+    const elemento = document.getElementById('#nome_usuario');
+    let nome = sessionStorage.getItem(NOME_USUARIO);
+    if(nome == null){
+        elemento.style.display = "none"; 
+        return;
+    }
+    elemento.innerHTML = nome;
+    return;
+};
