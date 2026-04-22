269187d6abe07aeb99c1017985736745c96a99af
Caiqe <cttcaiquegomes@gmail.com>
2026-04-22 17:18:58 -0300

    Feat: iniciando padronização

--- src/routes/perfis.js
+++ src/routes/perfis.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-var perfilController = require("../controllers/perfilController");
+var perfilController = require("../controllers/perfisController");
 
 router.post('/cadastrar', function (req, res) {
     perfilController.cadastrar(req, res)
