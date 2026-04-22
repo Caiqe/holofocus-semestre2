b589d1dab127e63c0607111a638f4186e4a112e9
VictorSdePasta <victor.delima@sptech.school>
2026-04-22 17:52:52 -0300

    fix: correção na digitação da rota backend e icone aba html

--- src/routes/perfis.js
+++ src/routes/perfis.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-var perfilController = require("../controllers/perfilController");
+var perfisController = require("../controllers/perfisController");
 
 router.post('/cadastrar', function (req, res) {
-    perfilController.cadastrar(req, res)
+    perfisController.cadastrar(req, res)
 })
 
 module.exports = router;
