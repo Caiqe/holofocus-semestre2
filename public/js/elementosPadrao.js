502d2cfbe5633fb226aef99b48ac6594cf12ecdc
Caiqe <cttcaiquegomes@gmail.com>
2026-04-25 11:50:58 -0300

    feat: coletando dados do login e repassando

--- public/js/elementosPadrao.js
+++ public/js/elementosPadrao.js
@@ -1,17 +1,41 @@
 const toggle = document.getElementById("menuHamburguer");
 const header = document.getElementById("header");
 
-toggle.addEventListener("change", function () {
-    header.classList.toggle("ativo");
-});
+if (toggle && header) {
+    toggle.addEventListener("change", function () {
+        header.classList.toggle("ativo");
+    });
+}
 
-const pegarNomeUsuario = () =>{
-    const elemento = document.getElementById('#nome_usuario');
-    let nome = sessionStorage.getItem(NOME_USUARIO);
-    if(nome == null){
-        elemento.style.display = "none"; 
-        return;
+
+
+
+
+  document.addEventListener('DOMContentLoaded', pegarNomeUsuario);
+
+const logout = () => {
+    sessionStorage.clear()
+    navigation.navigate("/")
+}
+
+const isAdm = () =>{
+    const permissao = sessionStorage.getItem('NIVEL_ACESSO');
+
+    if(permissao == null || permissao != '1'){
+        logout();
     }
-    elemento.innerHTML = nome;
     return;
-};
+}
+
+const validarSessao = () => {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    const elemento = document.getElementById('nome_usuario');
+
+    if (email != null && nome != null) {
+        elemento.innerHTML = nome;
+    } else {
+        logout();
+    }
+}
