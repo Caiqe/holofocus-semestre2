f0d31e6174b213325f989d1f7a1a81e68ef0cdc9
Lucas-T-Neves <lucas.neves@sptech.school>
2026-05-23 20:44:16 -0300

    feat:implementação Slack

--- public/api-logs/src/main/java/school/sptech/slack/Cliente.java
+++ public/api-logs/src/main/java/school/sptech/slack/Cliente.java
@@ -1,8 +1,13 @@
 package school.sptech.slack;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Cliente extends Usuario {
 
-    private static final String URL = "https://hooks.slack.com/services/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";
+    private static final String URL = Dotenv.configure()
+            .directory("C:/Users/Lneve/Downloads/githolofocus/holofocus-semestre2")
+            .load()
+            .get("SLACK_CLIENTE_URL");
 
     public Cliente(String nome, String email) {
         super(nome, email, URL);
--- public/api-logs/src/main/java/school/sptech/slack/Suporte.java
+++ public/api-logs/src/main/java/school/sptech/slack/Suporte.java
@@ -1,8 +1,13 @@
 package school.sptech.slack;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Suporte extends Usuario {
 
-    private static final String URL = "https://hooks.slack.com/services/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";
+    private static final String URL = Dotenv.configure()
+            .directory("C:/Users/Lneve/Downloads/githolofocus/holofocus-semestre2")
+            .load()
+            .get("SLACK_SUPORTE_URL");
 
     public Suporte(String nome, String email) {
         super(nome, email, URL);
