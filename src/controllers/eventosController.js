200196e2b194c719a9915e64a31f13ea012d4abe
Manu Prado <manuela.garcia@sptech.school>
2026-06-05 21:15:19 -0300

    subindo alterações

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -3,11 +3,14 @@ var eventosModel = require('../models/eventosModel');
 function criar(req, res) {
     const evento = req.body;
 
-    if (!evento.nome || !evento.data) {
+    if (!evento.nomeEvento || !evento.data) {
         return res.status(400).send("Dados incompletos");
     }
 
-    eventosModel.criarEvento(evento)
+    eventosModel.criarEvento(evento.nomeEvento,
+    evento.data,
+    evento.artista,
+    evento.genero)
         .then(resultado => res.status(201).json(resultado))
         .catch(erro => {
             console.log(erro);
@@ -31,7 +34,11 @@ function editar(req, res) {
     const id = req.params.id;
     const evento = req.body;
 
-    eventosModel.editarEvento(id, evento)
+    eventosModel.editarEvento( id,
+    evento.nomeEvento,
+    evento.data,
+    evento.artista,
+    evento.genero)
         .then(resultado => res.json(resultado))
         .catch(erro => {
             console.log(erro);
--- src/models/eventosModel.js
+++ src/models/eventosModel.js
@@ -1,13 +1,12 @@
 const { listar } = require('../controllers/eventosController');
 var database = require('../database/config');
 
-function criarEvento(nomeEvento, data, artista, genero,investimento,lucro,lotacaoCasa,lotacaoEvento) {
+function criarEvento(nomeEvento, data, artista, genero) {
     const instrucao = `
         INSERT INTO evento 
-        (nome_evento, data_evento, investimento_evento, retorno_evento, total_pessoas, fk_empresa, fk_artista, fk_genero)
+        (nome_evento, data_evento, fk_empresa, fk_artista, fk_genero)
         VALUES 
-        ('${nomeEvento}',${data},${investimento}, ${lucro} ,
-         ${lotacaoEvento},${artista},${genero});
+        ('${nomeEvento}','${data}', 1,'${artista}','${genero}');
     `;
     return database.executar(instrucao);
 }
@@ -17,28 +16,23 @@ function listarEvento() {
     const instrucao = `
         SELECT
             id_evento,
-            nome_evento,
-            data_evento,
-            investimento_evento,
-            retorno_evento,
-            total_pessoas
-        FROM evento;
+            nome_evento as nomeEvento,
+            titulo_genero as genero,
+            data_evento as data
+        FROM evento join genero on id_genero = fk_genero;
     `;
 
     return database.executar(instrucao);
 
 }
 
-function editarEvento(id,nomeEvento, data, artista, genero,investimento,lucro,lotacaoCasa,lotacaoEvento) {
+function editarEvento(id,nomeEvento, data, artista, genero) {
     const instrucao = `
         UPDATE evento SET
-            nome_evento = '${nomeEvento}',
-            data_evento = '${data}',
-            investimento_evento = ${investimento},
-            retorno_evento = ${lucro},
-            total_pessoas = ${lotacaoEvento},
-            fk_artista = ${artista},
-            fk_genero = ${genero}
+            nome_evento = '${nomeEvento}' as nome,
+            data_evento = '${data}' as data,
+            fk_artista = ${artista} artista,
+            fk_genero = ${genero} as genero
         WHERE id_evento = ${id};
     `;
     return database.executar(instrucao);
